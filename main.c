1230a577a0e1f856189d767e405057187781c77b
DarkElveon <sergej.follenweider@gmail.com>
2014-04-24 18:00:45 +0200

    update2

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
-tst
+/* 
+ * File:   main.c
+ * Author: Elveon
+ *
+ * Created on 24. April 2014, 16:38
+ */
 
+#include <stdio.h>
+#include <stdlib.h>
 
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Hallo Welt");
+    return (EXIT_SUCCESS);
+}
 
-hallo 
