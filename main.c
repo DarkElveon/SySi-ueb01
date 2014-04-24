556a864e29ab4ae4009a77c46b28ff58a75bddd7
DarkElveon <sergej.follenweider@gmail.com>
2014-04-24 21:16:45 +0200

    Aufgabe fertig!

--- main.c
+++ main.c
@@ -2,18 +2,27 @@
  * File:   main.c
  * Author: Elveon
  *
- * Created on 24. April 2014, 16:38
+ * Created on 24. April 2014, 18:20
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /*
- * 
+ * Gibt das aktuelle Datum mit Uhrzeit aus
  */
-int main(int argc, char** argv) {
-    printf("update1");
-    printf("update2")
-    return (EXIT_SUCCESS);
+int main() {
+    time_t zeit;
+    time(&zeit);                                // Sekunden seit 1. Jan 1970
+    struct tm *lokaleZeit = localtime(&zeit);   // Pointer vom Typ tm
+    char ausgabeZeit[MAX_SIZE];
+    
+    printf("Aktuelles Datum: %s\n", ctime(&zeit));
+
+    /* Ausgabe als String mit Formatieranweisungen */
+    strftime(ausgabeZeit, MAX_SIZE, "%a %b %d %H:%M:%S %Y", lokaleZeit);
+    
+    printf("Aktuelles Datum: %s\n", ausgabeZeit);
+    
+    return 0;
 }
 
---
+++ main.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.h
+ * Author: Elveon
+ *
+ * Created on 24. April 2014, 18:21
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX_SIZE 100
+
+#endif	/* MAIN_H */
+
